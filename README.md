262cc4f54d53a4366ff9db273574e6d9657594bf
Jarret Bryan <36285885+jarretbryan@users.noreply.github.com>
2023-07-01 21:29:51 -0400

    JJB-001 - Initial Commit
    * first run update
    
    * add in time since updated; rename card
    
    * readme fluff
    
    * to do

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-It's me, Tierney!
+Jarret has an NPX Business Card
 
 # Usage
 
 ## npm
 ```
-npx bitandbang
+npx jaybeecard
 ```
 
+### TODO: 
 ## GitHub Package Registry
 Assuming you have the GitHub registry set up, you can use via npx:
 ```
-npx @bnb/card
+npx @jarretbryan/jaybeecard
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- build.js
+++ build.js
@@ -13,51 +13,65 @@ const options = {
   borderStyle: 'round'
 }
 
+// DateTime Stuff
+
+const dateCalculator = {
+  dateDifference: function dateDifference (date) {
+    let difference = new Date().getTime() - new Date(date).getTime();
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+  }
+}
+
+const dates = {
+  updateDate: new Date('07/01/2023')
+}
+
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Principal Developer Advocate at Twilio (::)'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  mastodon: chalk.gray('https://mastodon.social/') + chalk.magenta('@bnb'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('           🐝 / Jarret Bryan'),
+  handle: chalk.white('jaybeekeeper'),
+  work: chalk.white('Creative Technologist + Software Engineer at Unite Us'),
+  bsky: chalk.gray('https://bsky.app/profile/') + chalk.cyan('jaybeekeeper.bsky.social'),
+  mastodon: chalk.gray('https://mastodon.social/') + chalk.magenta('@jaybeekeeper'),
+  github: chalk.gray('https://github.com/') + chalk.green('jarretbryan'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('jarretbryan'),
+  web: chalk.cyan('https://jarretbryan.online'),
+  npx: chalk.red('npx') + ' ' + chalk.white('jaybeecard'),
+  dates: chalk.blue.bold(`    Last Updated ${dateCalculator.dateDifference(dates.updateDate)} days ago.`),
   labelWork: chalk.white.bold('    Work:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
+  labelBsky: chalk.white.bold(' Bluesky:'),
   labelMastodon: chalk.white.bold('Mastodon:'),
-  labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
   labelLinkedIn: chalk.white.bold('LinkedIn:'),
   labelWeb: chalk.white.bold('     Web:'),
-  labelCard: chalk.white.bold('    Card:')
+  labelCard: chalk.white.bold('    Card:'),
+
 }
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
+const heading = `${data.name} / ${data.handle} `
 const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
+const bskying = `${data.labelBsky}  ${data.bsky}`
 const mastodoning = `${data.labelMastodon}  ${data.mastodon}`
-const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
+const dating = `${data.dates}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
+               bskying + newline + // data.labelTwitter + data.twitter
                mastodoning + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
                webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+               carding + newline + newline + // data.labelCard + data.npx
+               dating 
+
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
